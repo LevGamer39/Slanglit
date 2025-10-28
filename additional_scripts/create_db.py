9d5c8cbef19dab6141418e1fa399ea6878becf1b
LevGamer39 <Levk039@yandex.ru>
2025-10-28 11:54:14 +0200

    1

--- additional_scripts/create_db.py
+++ additional_scripts/create_db.py
@@ -18,7 +18,7 @@ def get_table_names_from_sql(file_path='sq_db.sql'):
         return []
 
 def create_database():
-    conn = sqlite3.connect('../database/translations.db')
+    conn = sqlite3.connect('../basic_scripts/translations.db')
     cursor = conn.cursor()
     
     with open('sq_db.sql', 'r', encoding='utf-8') as f:
